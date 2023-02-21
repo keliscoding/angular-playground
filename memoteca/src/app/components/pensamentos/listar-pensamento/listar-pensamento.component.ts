d0fe485536cabc35c24ea0982d690ec90f281e9e
Kelly Castelo <kawokkun@gmail.com>
2023-02-21 13:52:09 -0300

    modificando o titulo

--- memoteca/src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -15,6 +15,7 @@ export class ListarPensamentoComponent implements OnInit {
   filtro: string = '';
   favoritos: boolean = false;
   listaFavoritos: Pensamento[] = [];
+  titulo: string = 'Meu Mural';
 
   constructor(private service: PensamentoService, private router: Router) {}
 
@@ -50,6 +51,7 @@ export class ListarPensamentoComponent implements OnInit {
   }
 
   listarFavoritos() {
+    this.titulo = 'Meus Favoritos';
     this.favoritos = true;
     this.haMaisPensamentos = true;
     this.paginaAtual = 1;
