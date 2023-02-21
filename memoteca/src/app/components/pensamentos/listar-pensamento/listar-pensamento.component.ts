5e5c9a07e536b38e5db705e6d5be4fc593530864
Kelly Castelo <kawokkun@gmail.com>
2023-02-21 12:09:23 -0300

    excluindo um favorito da lista

--- memoteca/src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -13,6 +13,7 @@ export class ListarPensamentoComponent implements OnInit {
   haMaisPensamentos: boolean = true;
   filtro: string = '';
   favoritos: boolean = false;
+  listaFavoritos: Pensamento[] = [];
 
   constructor(private service: PensamentoService) {}
 
@@ -55,6 +56,7 @@ export class ListarPensamentoComponent implements OnInit {
       .listar(this.paginaAtual, this.filtro, this.favoritos)
       .subscribe((listaPensamentosFavoritos) => {
         this.listaPensamentos = listaPensamentosFavoritos;
+        this.listaFavoritos = listaPensamentosFavoritos;
       });
   }
 }
