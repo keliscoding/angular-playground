98253e08e9cbfabe50599119e8333943782a4261
Kelly Castelo <kawokkun@gmail.com>
2023-03-01 09:25:58 -0300

    renderizando os cards

--- projeto-buscante/src/app/views/lista-livros/lista-livros.component.ts
+++ projeto-buscante/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { Livro } from 'src/app/models/interfaces';
 import { LivroService } from 'src/app/service/livro.service';
+import { LivrosResultado, VolumeInfo } from '../../models/interfaces';
 
 @Component({
   selector: 'app-lista-livros',
@@ -8,18 +10,42 @@ import { LivroService } from 'src/app/service/livro.service';
   styleUrls: ['./lista-livros.component.css'],
 })
 export class ListaLivrosComponent implements OnDestroy {
-  listaLivros: [];
+  listaLivros: Livro[];
   campoBusca: string = '';
   subscription: Subscription;
+  livro: Livro;
 
   constructor(private service: LivroService) {}
 
   buscarLivros() {
     this.subscription = this.service.buscar(this.campoBusca).subscribe({
-      next: (retorno) => console.log(),
+      next: (items) => {
+        this.listaLivros = this.livrosResultadoParaLivros(items);
+      },
       error: (err) => console.error(err),
-      complete: () => console.log('Observable completado'),
     });
+  }
+
+  livrosResultadoParaLivros(items): Livro[] {
+    const livros: Livro[] = [];
+
+    items.forEach((item) => {
+      livros.push(
+        (this.livro = {
+          title: item.volumeInfo?.title,
+          authors: item.volumeInfo?.authors,
+          publisher: item.volumeInfo?.publisher,
+          publishedDate: item.volumeInfo?.publishedDate,
+          description: item.volumeInfo?.description,
+          previewLink: item.volumeInfo?.previewLink,
+          thumbnail: item.volumeInfo?.imageLinks?.thumbnail,
+        })
+      );
+    });
+
+    console.log(livros);
+
+    return livros;
   }
 
   ngOnDestroy() {
