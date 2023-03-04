fef6c4c54af2541115bffe82039fa2b4277a41ef
Kelly Castelo <kawokkun@gmail.com>
2023-03-04 07:32:55 -0300

    SwitchMap

--- projeto-buscante/src/app/views/lista-livros/lista-livros.component.ts
+++ projeto-buscante/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subscription, map, switchMap } from 'rxjs';
 import { Item, Livro } from 'src/app/models/interfaces';
 import { LivroService } from 'src/app/service/livro.service';
 import { LivrosResultado, VolumeInfo } from '../../models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
+import { FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-lista-livros',
@@ -12,20 +14,28 @@ import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 })
 export class ListaLivrosComponent implements OnDestroy {
   listaLivros: Livro[];
-  campoBusca: string = '';
+  campoBusca = new FormControl();
   subscription: Subscription;
   livro: Livro;
 
   constructor(private service: LivroService) {}
 
-  buscarLivros() {
+  // é convensao da comunidade botar um $ no final de um atributo que representa um observable
+  livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    map((items) => {
+      this.listaLivros = this.livrosResultadoParaLivros(items);
+    })
+  );
+
+  /*   buscarLivros() {
     this.subscription = this.service.buscar(this.campoBusca).subscribe({
       next: (items) => {
         this.listaLivros = this.livrosResultadoParaLivros(items);
       },
       error: (err) => console.error(err),
     });
-  }
+  } */
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
     return items.map((item) => {
